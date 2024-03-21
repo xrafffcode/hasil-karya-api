97ce253d1ed35ca1eed9fb20643c3a99a70066d7
xrafffcode <yusfiwawan2@gmail.com>
2024-03-21 15:47:27 +0700

    Refactor getStatisticTruckPerDayByStation method in MaterialMovementRepository.php

--- app/Repositories/MaterialMovementRepository.php
+++ app/Repositories/MaterialMovementRepository.php
@@ -94,6 +94,7 @@ class MaterialMovementRepository implements MaterialMovementRepositoryInterface
 
         $result = $result->map(function ($item) {
             $item['station'] = Station::find($item['station'])->name;
+            $item['value'] = $item['value'] * 1;
 
             return $item;
         });
